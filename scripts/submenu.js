1576edb67d333d122dd0125658ee25b20cfeac26
Asbjørn Kallevik <asbjorn.kallevik@gmail.com>
2019-11-26 18:23:36 +0100

    Adding transition to show submenu

--- scripts/submenu.js
+++ scripts/submenu.js
@@ -4,9 +4,21 @@
 	const shopButton = document.querySelector('#shop-button'),
 		shopSubmenu = document.querySelector('#shop-submenu');
 
-		shopButton.addEventListener('mouseover', function() {
-			document.querySelector('#shop-submenu').classList.toggle('showSubMenu');
+	// Show submenu on mouseover
+	shopButton.addEventListener('mouseover', function() {
+		shopSubmenu.classList.add('showSubMenu');
+
+		shopButton.addEventListener('mouseleave', function() {
+		//document.querySelector('#shop-submenu').classList.toggle('showSubMenu');
+			//setTimeout(function() {
+			//	shopSubmenu.classList.remove('showSubMenu');
+			//}, 1000);
+			
 		});
+	});
+
+	
+
 })();
 
 // After pseudo class to shop link needed (with down arrow)
