a124658ad45d19474e9a3040e891d74908c121b3
Asbjørn Kallevik <asbjorn.kallevik@gmail.com>
2019-12-01 01:58:49 +0100

    Fixed Shop submenu for mobile

--- scripts/submenu.js
+++ scripts/submenu.js
@@ -1,15 +1,24 @@
 (function() {
 	const shopButton = document.querySelector('#shop-button'),
-		shopSubmenu = document.querySelector('#shop-submenu');
+		shopSubmenu = document.querySelector('#shop-submenu'),
+		screen = window.matchMedia("(max-width: 799px)");
 
-	// Show submenu on mouseover
-	shopButton.addEventListener('mouseover', function() {
-		shopSubmenu.classList.add('showSubMenu');
-		// Hide submenu on mouseleave
-		shopButton.addEventListener('mouseleave', function() {
-			setTimeout(function() {
-				shopSubmenu.classList.remove('showSubMenu');
-			}, 400);
+	// Show submenu on mouseover, or click on smaller screens
+	if (!screen.matches) {
+		shopButton.addEventListener('mouseover', function() {
+			shopSubmenu.classList.add('showSubMenu');
+			// Hide submenu on mouseleave
+			shopButton.addEventListener('mouseleave', function() {
+				setTimeout(function() {
+					shopSubmenu.classList.remove('showSubMenu');
+				}, 400);
+			});
 		});
-	});
+	} else {
+		shopButton.addEventListener('click', function(e) {
+			e.preventDefault();
+			shopSubmenu.classList.toggle('showSubMenu');
+		});
+	}
+
 })();
